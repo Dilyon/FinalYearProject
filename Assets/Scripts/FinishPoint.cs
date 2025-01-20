e514ff52058eecf2402e219ecc30dbf9e2cb907a
Dilyon <dilyonong@gmail.com>
2025-01-20 17:13:11 +0800

    Unlock and resetting game progress

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishPoint : MonoBehaviour
 {
@@ -11,18 +12,33 @@ public class FinishPoint : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            //UnlockNewLevel();
+            UnlockNewLevel();
             SceneController.instance.NextLevel();
         }
     }
 
-    /**void UnlockNewLevel()
+    void UnlockNewLevel()
     {
+        int currentUnlocked = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        Debug.Log($"Before unlock - UnlockedLevel: {currentUnlocked}");
+
         if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
         {
             PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
             PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
             PlayerPrefs.Save();
+
+            Debug.Log($"After unlock - UnlockedLevel: {PlayerPrefs.GetInt("UnlockedLevel")}");
+            Debug.Log($"Current scene build index: {SceneManager.GetActiveScene().buildIndex}");
         }
-    }**/
+        else
+        {
+            Debug.Log("Level already unlocked - no changes made");
+        }
+    }
+    void Reset()
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", 5);  // Or whatever number you want
+        PlayerPrefs.Save();
+    }
 }
--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -3,22 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 public class LevelMenu : MonoBehaviour
 {
     public Button[] buttons;
-
     private void Awake()
     {
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        Debug.Log($"UnlockedLevel: {unlockedLevel}");
+        Debug.Log($"Total buttons: {buttons.Length}");
+
+        // First disable all buttons
         for (int i = 0; i < buttons.Length; i++)
         {
             buttons[i].interactable = false;
+            Debug.Log($"Button {i} initially set to: {buttons[i].interactable}");
         }
         for (int i = 0; i < unlockedLevel; i++)
         {
             buttons[i].interactable = true;
+            Debug.Log($"Button {i} enabled, now set to: {buttons[i].interactable}");
         }
+    }
+
+    public void ResetAllProgress()
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", 1);  // Reset to only level 1 unlocked
+        PlayerPrefs.SetInt("ReachedIndex", 1);   // Reset reached index
+        PlayerPrefs.Save();
+
+        // Refresh the level buttons immediately
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        buttons[0].interactable = true;  // Only first level interactable
+
+        Debug.Log("Game progress reset - only Level 1 is now unlocked");
+    }
+
+    // Add this method to help reset if needed
+    public void ResetLevels()
+    {
+        PlayerPrefs.SetInt("UnlockedLevel", 5); // Change this number to test different values
+        PlayerPrefs.Save();
+        Debug.Log("Levels reset - UnlockedLevel is now: " + PlayerPrefs.GetInt("UnlockedLevel"));
     }
 
     public void OpenLevel(int levelId)
